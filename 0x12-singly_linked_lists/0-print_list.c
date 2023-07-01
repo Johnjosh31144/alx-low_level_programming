f2ba2722dd51058655bef465b69c00afe67c642c
Johnjosh31144 <Sadsoul31144@gmail.com>
2023-07-01 11:56:25 -0700

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
-
-/**
-* print_list - check the code for Holberton School students.
-* @h: name of the list
-* Return: the number of nodes.
-*/
 size_t print_list(const list_t *h)
 {
    int count = 0;
@@ -29,4 +22,3 @@ size_t print_list(const list_t *h)
    }
    return (count);
 }
-
